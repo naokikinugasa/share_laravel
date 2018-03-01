ba68e2ce7d5a251376ecaf3160a9abfbc824a62e
naokikinugasa <s1410568@u.tsukuba.ac.jp>
2018-03-01 14:24:51 +0900

    人気商品変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -101,79 +101,55 @@
 <section class="container padding-top-3x padding-bottom-3x">
     <h3 class="text-center mb-30">人気の商品</h3>
     <div class="owl-carousel" data-owl-carousel="{ &quot;nav&quot;: false, &quot;dots&quot;: true, &quot;margin&quot;: 30, &quot;responsive&quot;: {&quot;0&quot;:{&quot;items&quot;:1},&quot;576&quot;:{&quot;items&quot;:2},&quot;768&quot;:{&quot;items&quot;:3},&quot;991&quot;:{&quot;items&quot;:4},&quot;1200&quot;:{&quot;items&quot;:4}} }">
-        <!-- Product-->
-        <div class="grid-item">
-            <div class="product-card">
-                <div class="product-badge text-danger">22% Off</div><a class="product-thumb" href="shop-single.html"><img src="img/shop/products/09.jpg" alt="Product"></a>
-                <h3 class="product-title"><a href="shop-single.html">Rocket Dog</a></h3>
-                <h4 class="product-price">
-                    <del>$44.95</del>$34.99
-                </h4>
-                <div class="product-buttons">
-                    <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist"><i class="icon-heart"></i></button>
-                    <button class="btn btn-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">Add to Cart</button>
+        <div class="grid-item" style="height: 320px;">
+            <div class="product-card" style="height: 320px;">
+                <a class="product-thumb" style="height: 150px;" href="/products/19"><img src="/img/product/19/thum.jpeg" style="width: 100%; height: 100%;"></a>
+                <h3 class="product-title" style="padding-top: 10px;"><a href="/products/19">Gear VR</a></h3>
+                <h4 class="product-price">¥ 200/日</h4>
+                <div class="product-buttons" style="padding-top: 1px;">
+                    <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="いいね"><i class="icon-heart"></i></button>
+                    <a href="/products/19">
+                        <button class="btn btn-outline-primary btn-sm">詳細を見る</button>
+                    </a>
                 </div>
             </div>
         </div>
-        <!-- Product-->
-        <div class="grid-item">
-            <div class="product-card">
-                <div class="rating-stars"><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star"></i>
-                </div><a class="product-thumb" href="shop-single.html"><img src="img/shop/products/03.jpg" alt="Product"></a>
-                <h3 class="product-title"><a href="shop-single.html">Oakley Kickback</a></h3>
-                <h4 class="product-price">$155.00</h4>
-                <div class="product-buttons">
-                    <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist"><i class="icon-heart"></i></button>
-                    <button class="btn btn-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">Add to Cart</button>
+        <div class="grid-item" style="height: 320px;">
+            <div class="product-card" style="height: 320px;">
+                <a class="product-thumb" style="height: 150px;" href="/products/8"><img src="/img/product/8/thum.jpeg" style="width: 100%; height: 100%;"></a>
+                <h3 class="product-title" style="padding-top: 10px;"><a href="/products/8">ハンドミキサー</a></h3>
+                <h4 class="product-price">¥ 100/日</h4>
+                <div class="product-buttons" style="padding-top: 1px;">
+                    <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="いいね"><i class="icon-heart"></i></button>
+                    <a href="/products/8">
+                        <button class="btn btn-outline-primary btn-sm">詳細を見る</button>
+                    </a>
                 </div>
             </div>
         </div>
-        <!-- Product-->
-        <div class="grid-item">
-            <div class="product-card"><a class="product-thumb" href="shop-single.html"><img src="img/shop/products/12.jpg" alt="Product"></a>
-                <h3 class="product-title"><a href="shop-single.html">Vented Straw Fedora</a></h3>
-                <h4 class="product-price">$49.50</h4>
-                <div class="product-buttons">
-                    <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist"><i class="icon-heart"></i></button>
-                    <button class="btn btn-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">Add to Cart</button>
+        <div class="grid-item" style="height: 320px;">
+            <div class="product-card" style="height: 320px;">
+                <a class="product-thumb" style="height: 150px;" href="/products/5"><img src="/img/product/5/thum.jpeg" style="width: 100%; height: 100%;"></a>
+                <h3 class="product-title" style="padding-top: 10px;"><a href="/products/5">プロジェクター</a></h3>
+                <h4 class="product-price">¥ 100/日</h4>
+                <div class="product-buttons" style="padding-top: 1px;">
+                    <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="いいね"><i class="icon-heart"></i></button>
+                    <a href="/products/5">
+                        <button class="btn btn-outline-primary btn-sm">詳細を見る</button>
+                    </a>
                 </div>
             </div>
         </div>
-        <!-- Product-->
-        <div class="grid-item">
-            <div class="product-card">
-                <div class="rating-stars"><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i><i class="icon-star filled"></i>
-                </div><a class="product-thumb" href="shop-single.html"><img src="img/shop/products/11.jpg" alt="Product"></a>
-                <h3 class="product-title"><a href="shop-single.html">Top-Sider Fathom</a></h3>
-                <h4 class="product-price">$90.00</h4>
-                <div class="product-buttons">
-                    <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist"><i class="icon-heart"></i></button>
-                    <button class="btn btn-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">Add to Cart</button>
-                </div>
-            </div>
-        </div>
-        <!-- Product-->
-        <div class="grid-item">
-            <div class="product-card"><a class="product-thumb" href="shop-single.html"><img src="img/shop/products/04.jpg" alt="Product"></a>
-                <h3 class="product-title"><a href="shop-single.html">Waist Leather Belt</a></h3>
-                <h4 class="product-price">$47.00</h4>
-                <div class="product-buttons">
-                    <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist"><i class="icon-heart"></i></button>
-                    <button class="btn btn-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">Add to Cart</button>
-                </div>
-            </div>
-        </div>
-        <!-- Product-->
-        <div class="grid-item">
-            <div class="product-card">
-                <div class="product-badge text-danger">50% Off</div><a class="product-thumb" href="shop-single.html"><img src="img/shop/products/01.jpg" alt="Product"></a>
-                <h3 class="product-title"><a href="shop-single.html">Unionbay Park</a></h3>
-                <h4 class="product-price">
-                    <del>$99.99</del>$49.99
-                </h4>
-                <div class="product-buttons">
-                    <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="Whishlist"><i class="icon-heart"></i></button>
-                    <button class="btn btn-primary btn-sm" data-toast data-toast-type="success" data-toast-position="topRight" data-toast-icon="icon-circle-check" data-toast-title="Product" data-toast-message="successfuly added to cart!">Add to Cart</button>
+        <div class="grid-item" style="height: 320px;">
+            <div class="product-card" style="height: 320px;">
+                <a class="product-thumb" style="height: 150px;" href="/products/22"><img src="/img/product/22/thum.jpeg" style="width: 100%; height: 100%;"></a>
+                <h3 class="product-title" style="padding-top: 10px;"><a href="/products/22">ニンテンドー64</a></h3>
+                <h4 class="product-price">¥ 100/日</h4>
+                <div class="product-buttons" style="padding-top: 1px;">
+                    <button class="btn btn-outline-secondary btn-sm btn-wishlist" data-toggle="tooltip" title="いいね"><i class="icon-heart"></i></button>
+                    <a href="/products/22">
+                        <button class="btn btn-outline-primary btn-sm">詳細を見る</button>
+                    </a>
                 </div>
             </div>
         </div>
