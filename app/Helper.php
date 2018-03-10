48c97805af5f5a88a4932549d701598565c7b2f2
naokikinugasa <s1410568@u.tsukuba.ac.jp>
2018-03-10 18:12:26 +0900

    料金

--- app/Helper.php
+++ app/Helper.php
@@ -39,14 +39,23 @@ class Helper
         if (isset($week) && $days >= 7) {
             if (isset($month) && $days >= 30) {
                 $price1 = (floor($days/30) * $month);
+                $price2 = 0;
                 if ($days % 30 >= 7) {
-                    $price2 = (floor(($days % 30)/7) * $week) + ((($days-1) % 30) % 7 * $day);
+                    $price2 = (floor(($days % 30)/7) * $week);
+                    if ((($days % 30) % 7) !== 0) {
+                        ((($days % 30) % 7) - 1) * $day;
+                    }
                 } else {
-                    $price2 = (($days-1) % 30 * $day);
+                    if (($days%30) !== 0) {
+                        $price2 = ((($days%30) - 1) * $day);
+                    }
                 }
                 $price = $price1 + $price2;
             } else {
-                $price = (floor($days/7) * $week) + (($days-1) % 7 * $day);
+                $price = (floor($days/7) * $week);
+                if (($days % 7) !== 0) {
+                    $price += (($days%7 - 1) * $day);
+                }
             }
         } else {
             $price = ($days-1) * $day;
