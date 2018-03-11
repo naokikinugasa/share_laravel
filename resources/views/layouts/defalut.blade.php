c52e8d544cfd21a5392f1df53069f9cfb56c25a8
naokikinugasa <s1410568@u.tsukuba.ac.jp>
2018-03-11 16:03:18 +0900

    メニューバー消えてたの表示

--- resources/views/layouts/defalut.blade.php
+++ resources/views/layouts/defalut.blade.php
@@ -442,7 +442,7 @@
     <div class="site-branding">
         <div class="inner">
             {{--<!-- Off-Canvas Toggle (#shop-categories)--><a class="offcanvas-toggle cats-toggle" href="#shop-categories" data-toggle="offcanvas"></a>--}}
-            {{--<!-- Off-Canvas Toggle (#mobile-menu)--><a class="offcanvas-toggle menu-toggle" href="#mobile-menu" data-toggle="offcanvas"></a>--}}
+            <!-- Off-Canvas Toggle (#mobile-menu)--><a class="offcanvas-toggle menu-toggle" href="#mobile-menu" data-toggle="offcanvas"></a>
             <!-- Site Logo--><a class="site-logo" href="/"><img src="/img/share.png"></a>
         </div>
     </div>
