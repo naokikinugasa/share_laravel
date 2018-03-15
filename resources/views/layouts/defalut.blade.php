7b98be2d1a56006155302d226409d71b895d1f54
naokikinugasa <s1410568@u.tsukuba.ac.jp>
2018-03-15 17:04:35 +0900

    ogt4

--- resources/views/layouts/defalut.blade.php
+++ resources/views/layouts/defalut.blade.php
@@ -62,7 +62,7 @@
     </script>
 
     <meta property="og:title" content="Share" />
-    <meta property="og:image" content="/img/ogt2.png" />
+    <meta property="og:image" content="/img/ogt4.png" />
 
 </head>
 <!-- Body-->
